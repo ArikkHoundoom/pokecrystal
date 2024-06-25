d5059c842766e3dce17bf2e6d3b087c39650a595
ArikkHoundoom <arikkhoundoom@gmail.com>
2024-06-24 22:31:25 -0500

    Added berry fanfare (unused for now); fixing kanto trainer battle music (attempt 1)
    -Berry fanfare added to SFX audio and constants, but not implemented.
    -"berries.asm" created, but not included at compilation time for now (no code calling it for now)
    -Attempt to fix audio on Kanto Trainer Battle Theme (circumventing engine limitations that make it not sound as expected)

--- audio/music/kantotrainerbattle.asm
+++ audio/music/kantotrainerbattle.asm
@@ -2,7 +2,6 @@
 ; Extended by ArikkHoundoom, partially based on the version from Pokémon HG/SS.
 ; https://github.com/ArikkHoundoom/pokecrystal/blob/master/audio/music/kantotrainerbattle.asm
 
-
 Music_KantoTrainerBattle:
 	channel_count 3
 	channel 1, Music_KantoTrainerBattle_Ch1
@@ -809,56 +808,50 @@ Music_KantoTrainerBattle_Ch1:
 	sound_ret
 
 Music_KantoTrainerBattle_Ch2:
-	transpose 1, 11
-	octave 4
 	note_type 12, 13, 1
 	vibrato 6, 3, 6
 	duty_cycle 1
-	note A#, 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
-	note G_, 1
-	note G#, 1
+	transpose 0, 0
+	octave 3
+	note A_, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
+	note F#, 1
 	note G_, 1
 	note F#, 1
+	note F_, 1
+	octave 4
 	duty_cycle 3
 	volume_envelope 6, 2
 .loop1:
 	octave 3
-	note G_, 1
-	note G#, 1
+	note F#, 1
 	note G_, 1
 	note F#, 1
-	sound_loop 14, .loop1
-	note G_, 1
-	note G#, 1
-	note G_, 1
 	note F_, 1
-	note D#, 1
+	sound_loop 14, .loop1
+	note F#, 1
+	note G_, 1
+	note F#, 1
+	note E_, 1
 	note D_, 1
-	note C_, 1
+	note C#, 1
 	octave 2
 	note B_, 1
+	note A#, 1
 	volume_envelope 13, 7
 	duty_cycle 2
-	note C_, 2
-	octave 3
-	note C_, 1
-	rest 1
+	octave 1
+	note B_, 2
 	octave 2
-	note C_, 1
+	note B_, 1
 	rest 1
 	octave 1
-	note A#, 2
-	octave 2
-	note A#, 1
-	rest 1
-	octave 1
-	note A#, 1
+	note B_, 1
 	rest 1
 	note A_, 2
 	octave 2
@@ -874,16 +867,23 @@ Music_KantoTrainerBattle_Ch2:
 	octave 1
 	note G#, 1
 	rest 1
-	note G_, 1
-	note B_, 1
+	note G_, 2
 	octave 2
-	note D_, 1
-	note F_, 1
 	note G_, 1
-	note B_, 1
+	rest 1
+	octave 1
+	note G_, 1
+	rest 1
+	note F#, 1
+	note A#, 1
+	octave 2
+	note C#, 1
+	note E_, 1
+	note F#, 1
+	note A#, 1
 	octave 3
-	note D_, 1
-	note F_, 1
+	note C#, 1
+	note E_, 1
 .mainloop:
 	octave 2
 	duty_cycle 2
@@ -891,73 +891,71 @@ Music_KantoTrainerBattle_Ch2:
 	note_type 12, 13, 7
 	sound_call .sub5
 	rest 1
-	octave 2
-	note C#, 1
-	note C#, 1
-	note G#, 1
-	rest 1
 	octave 3
-	note C#, 2
-	octave 2
-	note C#, 2
-	note G#, 1
+	note C_, 1
+	note C_, 1
+	note G_, 1
 	rest 1
+	octave 4
+	note C_, 2
 	octave 3
 	note C_, 2
-	octave 2
-	note C#, 2
-	note G#, 1
+	note G_, 1
 	rest 1
-	note A#, 2
-	note C#, 1
-	note C#, 1
-	note F_, 1
+	note B_, 2
+	note C_, 2
+	note G_, 1
+	rest 1
+	note A_, 2
+	note C_, 1
+	note C_, 1
+	note E_, 1
 	rest 1
 	note G#, 2
-	octave 3
-	note C#, 1
+	octave 4
+	note C_, 1
 	rest 1
-	note F_, 2
-	note G#, 1
+	note E_, 2
+	note G_, 1
 	rest 1
-	note A#, 2
+	note A_, 2
 	octave 2
 	sound_call .sub5
 	note_type 12, 13, 7
 	rest 1
-	octave 1
-	note A#, 1
-	note A#, 1
 	octave 2
-	note F_, 1
-	rest 1
-	note A#, 2
-	octave 1
-	note A#, 2
-	octave 2
-	note F_, 1
+	note A_, 1
+	note A_, 1
+	octave 3
+	note E_, 1
 	rest 1
 	note A_, 2
-	octave 1
-	note A#, 2
 	octave 2
-	note F_, 1
+	note A_, 2
+	octave 3
+	note E_, 1
 	rest 1
 	note G#, 2
-	octave 1
-	note A#, 1
-	note A#, 1
 	octave 2
-	note F_, 1
+	note A_, 2
+	octave 3
+	note E_, 1
 	rest 1
 	note G_, 2
-	note A#, 1
-	rest 1
+	octave 2
+	note A_, 1
+	note A_, 1
 	octave 3
-	note D_, 2
-	note F_, 1
+	note E_, 1
 	rest 1
-	note A#, 2
+	note F#, 2
+	note A_, 1
+	rest 1
+	octave 4
+	note C#, 2
+	note E_, 1
+	rest 1
+	note A_, 2
 	octave 1
 	sound_call .sub6
 	octave 1
@@ -965,115 +963,167 @@ Music_KantoTrainerBattle_Ch2:
 	vibrato 18, 2, 4
 	note_type 12, 7, 8
 	octave 4
-	note G_, 16
+	transpose 0, 0
+	note F#, 16
 	volume_envelope 7, 7
-	note G_, 16
+	note F#, 16
 	note_type 6, 8, 8
-	note G#, 16
+	note G_, 16
 	volume_envelope 8, 7
-	note G#, 15
-	note G_, 1
-	note G#, 7
-	note A_, 1
-	note A#, 3
-	note A_, 1
-	note G#, 8
-	note G_, 4
-	note F_, 6
+	note G_, 15
+	note F#, 1
+	note G_, 7
+	note G#, 1
+	note A_, 3
+	note G#, 1
+	note G_, 8
+	note F#, 4
+	note E_, 6
 	rest 2
 	note_type 12, 9, 8
-	note A_, 16
+	note G#, 16
 	volume_envelope 9, 7
-	note A_, 16
+	note G#, 16
 	volume_envelope 10, 7
-	note A#, 16
+	note A_, 16
 	volume_envelope 13, 7
 	octave 5
-	note D_, 8
-	note F_, 8
+	note C#, 8
+	note E_, 8
 	vibrato 4, 3, 6
 	octave 2
-	note A#, 4
-	note D#, 4
-	note C_, 1
+	note A_, 4
+	note D_, 4
+	octave 1
+	note B_, 1
+	octave 2
 	rest 1
 	volume_envelope 13, 8
-	note G#, 6
+	note G_, 6
 	volume_envelope 13, 7
-	note G#, 16
-	note A#, 4
-	note D#, 4
-	note D#, 1
+	note G_, 16
+	note A_, 4
+	note D_, 4
+	note D_, 1
 	rest 1
 	octave 3
-	note D#, 14
-	octave 2
-	note C_, 1
+	note D_, 14
+	octave 1
+	note B_, 1
 	rest 1
 	octave 3
-	note D#, 2
+	note D_, 2
 	rest 2
 	note_type 6, 13, 7
-	octave 2
-	note C_, 1
+	octave 1
+	note B_, 1
 	rest 1
-	note C_, 1
+	note B_, 1
 	rest 1
-	note C_, 2
+	note B_, 2
 	rest 2
-	note C_, 2
+	note B_, 2
 	rest 2
 	octave 3
-	note G#, 8
+	note G_, 8
 	octave 4
+	note E_, 2
 	note F_, 2
-	note F#, 2
-	note F_, 2
-	note D#, 2
-	note F_, 2
+	note E_, 2
+	note D_, 2
+	note E_, 2
 	rest 2
-	note D#, 2
+	note D_, 2
 	rest 2
 	octave 3
+	note E_, 2
+	rest 2
 	note F_, 2
 	rest 2
-	note F#, 2
+	note G_, 2
 	rest 2
-	note G#, 2
+	note C_, 2
 	rest 2
-	note C#, 2
-	rest 2
-	note G#, 2
-	note A#, 2
-	note G#, 2
-	note F#, 2
-	note G#, 2
+	note G_, 2
+	note A_, 2
+	note G_, 2
+	note F_, 2
+	note G_, 2
 	volume_envelope 7, 2
-	note G_, 1
-	note F_, 1
+	note F#, 1
 	note E_, 1
-	note D_, 1
-	note C_, 1
+	note D#, 1
+	note C#, 1
 	octave 2
 	note B_, 1
+	note A_, 1
 	volume_envelope 12, 1
 	octave 3
-	note B_, 2
+	note A#, 2
 	octave 1
-	note F#, 1
+	note F_, 1
 	rest 1
-	note F#, 1
+	note F_, 1
 	rest 1
 	octave 3
-	note B_, 2
+	note A#, 2
 	octave 1
-	note F#, 1
+	note F_, 1
 	rest 1
-	note F#, 1
+	note F_, 1
 	rest 1
 	volume_envelope 13, 7
 	octave 3
-	note B_, 4
+	note A#, 4
+	octave 1
+	note A_, 2
+	rest 2
+	octave 2
+	note E_, 2
+	rest 2
+	octave 1
+	note A_, 2
+	octave 2
+	note A_, 2
+	rest 2
+	note E_, 2
+	octave 1
+	note A_, 2
+	rest 2
+	octave 2
+	note F_, 2
+	rest 2
+	octave 1
+	note A_, 2
+	octave 2
+	note A_, 2
+	rest 2
+	note F_, 2
+	octave 1
+	note A_, 1
+	rest 1
+	note A_, 1
+	rest 3
+	note A_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 2
+	note A_, 2
+	rest 2
+	note F#, 2
+	octave 1
+	note A_, 2
+	rest 2
+	octave 2
+	note G_, 2
+	rest 2
+	octave 1
+	note A_, 2
+	octave 2
+	note A_, 2
+	rest 2
+	note A_, 2
 	octave 1
 	note A#, 2
 	rest 2
@@ -1124,153 +1174,103 @@ Music_KantoTrainerBattle_Ch2:
 	rest 2
 	note A#, 2
 	octave 1
-	note B_, 2
+	note A_, 2
 	rest 2
 	octave 2
-	note F#, 2
+	note E_, 2
 	rest 2
 	octave 1
-	note B_, 2
-	octave 2
-	note B_, 2
-	rest 2
-	note F#, 2
-	octave 1
-	note B_, 2
-	rest 2
-	octave 2
-	note G_, 2
-	rest 2
-	octave 1
-	note B_, 2
-	octave 2
-	note B_, 2
-	rest 2
-	note G_, 2
-	octave 1
-	note B_, 1
-	rest 1
-	note B_, 1
-	rest 3
-	note B_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	octave 2
-	note B_, 2
-	rest 2
-	note G#, 2
-	octave 1
-	note B_, 2
-	rest 2
+	note A_, 2
+	note A_, 2
 	octave 2
 	note A_, 2
 	rest 2
 	octave 1
-	note B_, 2
-	octave 2
-	note B_, 2
-	rest 2
-	note B_, 2
-	octave 1
-	note A#, 2
+	note A_, 2
 	rest 2
 	octave 2
 	note F_, 2
 	rest 2
 	octave 1
-	note A#, 2
-	note A#, 2
+	note A_, 2
+	note A_, 2
 	octave 2
-	note A#, 2
+	note A_, 2
 	rest 2
 	octave 1
-	note A#, 2
+	note A_, 2
 	rest 2
 	octave 2
 	note F#, 2
 	rest 2
 	octave 1
-	note A#, 2
-	note A#, 2
+	note A_, 2
+	note A_, 2
 	octave 2
-	note A#, 2
+	note A_, 2
 	rest 2
 	octave 1
-	note A#, 2
-	rest 2
-	octave 2
-	note G_, 2
-	rest 2
-	octave 1
-	note A#, 2
-	note A#, 2
-	octave 2
-	note A#, 2
-	rest 2
-	octave 1
-	note A#, 2
-	rest 2
-	octave 2
-	note G#, 2
-	rest 2
-	octave 1
-	note A#, 2
-	note A#, 2
-	octave 2
-	note A#, 2
-	octave 1
-	rest 2
-	note B_, 2
+	note A_, 2
 	rest 2
 	octave 2
 	note G_, 2
-	octave 1
-	rest 2
-	note B_, 2
-	note B_, 2
-	octave 2
-	note B_, 2
 	rest 2
 	octave 1
-	note B_, 2
-	rest 2
-	octave 2
-	note G#, 2
-	rest 2
-	octave 1
-	note B_, 2
-	note B_, 2
-	octave 2
-	note B_, 2
-	rest 2
-	octave 1
-	note B_, 2
-	rest 2
+	note A_, 2
+	note A_, 2
 	octave 2
 	note A_, 2
 	octave 1
 	rest 2
-	note B_, 2
-	note B_, 2
+	note A#, 2
+	rest 2
 	octave 2
-	note B_, 2
-	rest 2
+	note F#, 2
 	octave 1
-	note B_, 2
 	rest 2
+	note A#, 2
+	note A#, 2
 	octave 2
 	note A#, 2
 	rest 2
 	octave 1
-	note B_, 2
-	note B_, 2
+	note A#, 2
+	rest 2
 	octave 2
-	note B_, 2
+	note G_, 2
+	rest 2
+	octave 1
+	note A#, 2
+	note A#, 2
+	octave 2
+	note A#, 2
+	rest 2
+	octave 1
+	note A#, 2
+	rest 2
+	octave 2
+	note G#, 2
+	octave 1
+	rest 2
+	note A#, 2
+	note A#, 2
+	octave 2
+	note A#, 2
+	rest 2
+	octave 1
+	note A#, 2
+	rest 2
+	octave 2
+	note A_, 2
+	rest 2
+	octave 1
+	note A#, 2
+	note A#, 2
+	octave 2
+	note A#, 2
 	rest 2
 	note_type 12, 10, -7
 	duty_cycle 3
-	transpose 0, 0
 	octave 4
 	vibrato 18, 6, 1
 	note F_, 16
@@ -1458,6 +1458,7 @@ Music_KantoTrainerBattle_Ch2:
 
 .sub1:
 	duty_cycle 2
+	transpose 0, 0
 	note A_, 2
 	octave 2
 	note E_, 1
@@ -1506,114 +1507,127 @@ Music_KantoTrainerBattle_Ch2:
 	sound_ret
 
 .sub5:
-	transpose 1, 11
 	vibrato 6, 3, 6
 	duty_cycle 2
 	note_type 12, 13, 7
-	note C_, 1
-	note C_, 1
-	octave 3
-	note C_, 1
-	rest 1
 	octave 2
-	note G_, 1
-	rest 1
+	transpose 1, 0
+	note B_, 1
+	note B_, 1
 	octave 3
-	note C_, 1
-	rest 1
-	octave 2
-	note C_, 2
 	note B_, 1
 	rest 1
-	note G_, 1
+	note F#, 1
 	rest 1
 	note B_, 1
 	rest 1
-	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
 	note A#, 1
 	rest 1
-	note G_, 1
+	note F#, 1
 	rest 1
 	note A#, 1
 	rest 1
-	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
 	note A_, 1
 	rest 1
+	note F#, 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 2
+	note B_, 2
+	octave 3
 	note G#, 1
 	rest 1
+	note G_, 1
+	octave 4
+	rest 1
+	note D_, 1
 	octave 3
-	note C_, 1
 	sound_ret
 
 .sub6:
-	note F_, 2
 	octave 2
-	note F_, 1
-	rest 1
-	note C_, 1
-	rest 1
-	note F_, 1
-	rest 1
-	octave 1
-	note F_, 2
-	octave 2
+	note E_, 2
+	octave 3
 	note E_, 1
 	rest 1
-	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
 	rest 1
 	note E_, 1
 	rest 1
-	octave 1
-	note F_, 2
 	octave 2
+	note E_, 2
+	octave 3
 	note D#, 1
 	rest 1
-	note C_, 1
+	octave 2
+	note B_, 1
+	octave 3
 	rest 1
 	note D#, 1
 	rest 1
-	octave 1
-	note F_, 2
 	octave 2
+	note E_, 2
+	octave 3
 	note D_, 1
 	rest 1
-	note D#, 1
-	note C_, 1
-	octave 1
-	note A#, 1
+	octave 2
+	note B_, 1
+	octave 3
+	rest 1
+	note D_, 1
+	rest 1
+	octave 2
+	note E_, 2
+	octave 3
+	note C#, 1
+	rest 1
+	note D_, 1
+	octave 2
+	note B_, 1
 	note A_, 1
-	note A#, 1
+	note G#, 1
+	note A_, 1
 	rest 1
-	note A#, 1
+	note A_, 1
 	rest 1
-	octave 3
+	octave 4
+	note E_, 4
+	octave 2
+	note A_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 4
 	note F_, 4
-	octave 1
-	note A#, 1
+	octave 2
+	note A_, 1
 	rest 1
-	note A#, 1
+	note A_, 1
 	rest 1
-	octave 3
+	octave 4
 	note F#, 4
-	octave 1
-	note A#, 1
+	octave 2
+	note A_, 1
 	rest 1
-	note A#, 1
+	note A_, 1
 	rest 1
-	octave 3
+	octave 4
 	note G_, 4
-	octave 1
-	note A#, 1
-	rest 1
-	note A#, 1
-	rest 1
 	octave 3
-	note G#, 4
 	sound_ret
 
 .sub7:
-	transpose 1, 0
 	volume_envelope 11, 8
+	transpose 0, 0
 	note A#, 1
 	rest 1
 	note A#, 1
---
+++ data/items/berries.asm
@@ -0,0 +1,10 @@
+Berries:
+    db PSNCUREBERRY
+    db PRZCUREBERRY
+    db BURNT_BERRY
+    db ICE_BERRY
+    db BITTER_BERRY
+    db MINT_BERRY
+    db MIRACLEBERRY
+    db MYSTERYBERRY
+    db -1
